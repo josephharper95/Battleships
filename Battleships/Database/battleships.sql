137ce2df6dddb3fdf457e68d976e8d209e4fe5a8
Joseph Harper <josephharper@live.co.uk>
2016-10-03 13:36:25 +0100

    Updated .sql file to add first and last names Also added manual script to copy and paste to incrementally add the columns

--- Battleships/Database/battleships.sql
+++ Battleships/Database/battleships.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 30, 2016 at 11:48 PM
+-- Generation Time: Oct 03, 2016 at 02:34 PM
 -- Server version: 10.1.13-MariaDB
--- PHP Version: 7.0.4
+-- PHP Version: 7.0.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -119,6 +119,8 @@ CREATE TABLE `shotsfired` (
 CREATE TABLE `users` (
   `userID` varchar(20) NOT NULL,
   `password` char(64) NOT NULL,
+  `firstName` varchar(50) NOT NULL,
+  `lastName` varchar(50) NOT NULL,
   `saveGameID` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -126,9 +128,9 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`userID`, `password`, `saveGameID`) VALUES
-('test1', '1b4f0e9851971998e732078544c96b36c3d01cedf7caa332359d6f1d83567014', NULL),
-('user', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8', 2);
+INSERT INTO `users` (`userID`, `password`, `firstName`, `lastName`, `saveGameID`) VALUES
+('test1', '1b4f0e9851971998e732078544c96b36c3d01cedf7caa332359d6f1d83567014', '', '', NULL),
+('user', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8', '', '', 2);
 
 -- --------------------------------------------------------
 
