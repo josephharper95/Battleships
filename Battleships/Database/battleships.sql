dda91a0b543c92f27e0b20d7e630e30002adf49e
Aminul Hoque <hoquea2@aston.ac.uk>
2016-10-05 21:36:09 +0100

    Removed out of date insert user

--- Battleships/Database/battleships.sql
+++ Battleships/Database/battleships.sql
@@ -127,10 +127,6 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`userID`, `password`, `firstName`, `lastName`, `saveGameID`) VALUES
-('test1', '1b4f0e9851971998e732078544c96b36c3d01cedf7caa332359d6f1d83567014', '', '', NULL),
-('user', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8', '', '', 2);
-
 -- --------------------------------------------------------
 
 --
