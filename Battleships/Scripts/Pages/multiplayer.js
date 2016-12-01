4aa4b578e047679c8afcd9c1bd3aeb829a6ec97a
Dave <Dave@Daves-MBP.home>
2016-12-01 14:28:18 +0000

    Updated node pointer

--- Battleships/Scripts/Pages/multiplayer.js
+++ Battleships/Scripts/Pages/multiplayer.js
@@ -26,8 +26,8 @@
 
 // Connecting to socket.io //var socket = io.connect('http://40.68.102.207:3000');
 //var socket = io.connect('https://battleships.online:3000', {secure: true});
-//var socket = io.connect('https://battleships-preprod.tk:3000', {secure: true});
-var socket = io.connect('http://localhost:3000'); // UNCOMMENT FOR LOCALHOST DEV
+var socket = io.connect('https://battleships-preprod.tk:3000', {secure: true});
+//var socket = io.connect('http://localhost:3000'); // UNCOMMENT FOR LOCALHOST DEV
 
 
 var game;
