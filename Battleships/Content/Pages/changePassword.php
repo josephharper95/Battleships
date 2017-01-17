41d260742ffd0af4be77207cd26cb77021bdc868
Nick <Nick@Nicks-MacBook-Pro.local>
2017-01-17 23:44:17 +0000

    V2.10.1
    Mission List images updated

--- Battleships/Content/Pages/changePassword.php
+++ Battleships/Content/Pages/changePassword.php
@@ -17,12 +17,15 @@ if(!Session::get("userID")) {
     exit();
 }
 
-if (Input::itemExists("changePassword")) 
-{
-    if(Input::itemExists("oldPassword") && Input::itemExists("newPassword") && Input::itemExists("passwordMatch") && Input::post("newPassword"))
-    {
-        if(Input::post("newPassword") === Input::post("passwordMatch")) 
-        {
+if (Input::itemExists("changePassword"))  {
+
+    if (Input::itemExists("oldPassword") && 
+        Input::itemExists("newPassword") && 
+        Input::itemExists("passwordMatch") && 
+        Input::post("newPassword")) {
+
+        if (Input::post("newPassword") == Input::post("passwordMatch")) {
+
             $userQuery = new User();
             $userID = Session::get("userID");
             $hashedPassword = hash("sha256", Input::post("oldPassword"));
@@ -30,47 +33,78 @@ if (Input::itemExists("changePassword"))
 
             $userQuery->checkForUserAndPassword($userID, $hashedPassword); // Check to see if the entered password/username combination exists in the DB
 
-            if($userQuery->db->getRowCount() > 0) // If the old entered password matches the one in the DB...
-            {
-                $userQuery->updatePasswordByUserID($userID, $newHashedPassword);
+            // If the old entered password matches the one in the DB... 
+            if ($userQuery->db->getRowCount() > 0) {
 
+                $userQuery->updatePasswordByUserID($userID, $newHashedPassword);
                 Session::set("changePasswordMessage", "Password change successful.");
-            }
-            else
-            {
+            } else {
+
                 Session::set("changePasswordMessage", "Please ensure your old password is correct.");
             }
-        }
-        else
-        {
+        } else {
+
             Session::set("changePasswordMessage", "Please ensure both the new password and confirm password entries match.");
         }
-    }
-    else
-    {
+    } else {
+
         Session::set("changePasswordMessage", "Please ensure you have entered data in all the fields");
     }
 }
 
 // include the header file if it has not been included before
 require_once("header.php");
+
 ?>
 
-            <form method="post" action="">
-                <li><input type="password" placeholder="Old Password" name="oldPassword"/></li>
-                <li><input type="password" placeholder="New Password" name="newPassword" /></li>
-                <li><input type="password" placeholder="Confirm New Password" name="passwordMatch" /></li>
-                <li><button type="submit" name="changePassword"> Change Password </button></li>
+<link rel="stylesheet" type="text/css" href="../Styles/Pages/changePassword.css" />
 
-                        <li class="changePasswordError">
-                            <?= Session::get("changePasswordMessage"); ?>
-                        </li>
+<div id="pageChangePasswordCont"
+        class="pageContainer">
 
-                    </ul>
+    <div id="pageChangePassword"
 
-            </form>
+    <?php
+        if (Session::exists("changePasswordMessage")) {
+            echo "class='extra'";
+        }
+    ?>
 
+    >
+
+        <h1>Change Password</h1>
+
+        <form method="post" action="">
+
+            <ul class="blank">
+                
+                <li>
+                    <input type="password" placeholder="Old Password" name="oldPassword"/>
+                </li>
+
+                <li>
+                    <input type="password" placeholder="New Password" name="newPassword" />
+                </li>
+
+                <li>
+                    <input type="password" placeholder="Confirm New Password" name="passwordMatch" />
+                </li>
+
+                <li>
+                    <button type="submit" name="changePassword"> Change Password </button>
+                </li>
+
+                <li class="changePasswordError">
+                    <?= Session::get("changePasswordMessage"); ?>
+                </li>
+            </ul>
+        </form>
+    </div>
+</div>
 
 <?php
+
+require_once("footer.php");
 Session::delete("changePasswordMessage");
+
 ?>
