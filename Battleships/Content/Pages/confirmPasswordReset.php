2df14a837f115fc8be42504bfc2b03f02148c1fa
Azure Development Server <josephharper@live.co.uk>
2017-01-18 10:40:39 +0000

    Version 2.10.4 Password reset functionality

--- Battleships/Content/Pages/confirmPasswordReset.php
+++ Battleships/Content/Pages/confirmPasswordReset.php
@@ -17,11 +17,77 @@ if(Session::get("userID")) {
     exit();
 }
 
+if(Input::itemExists("passwordSetAttempt"))
+{
+    if (Input::itemExists("newPassword") && 
+        Input::itemExists("passwordMatch") && 
+        Input::post("newPassword") && 
+        Input::itemExists("userID")) {
+
+        if (Input::post("newPassword") == Input::post("passwordMatch")) {
+
+            $userQuery = new User();
+            $userID = Input::post("userID");
+            $newHashedPassword = hash("sha256", Input::post("newPassword"));
+
+            // Delete reset code/user id combination from db
+            $userQuery->deletePendingPasswordReset($userID);
+            $userQuery->updatePasswordByUserID($userID, $newHashedPassword);
+            Session::set("loginMessage", "Password change successful. Please enter your username and password to log in.");
+            header("Location: login.php");
+            exit();
+        } else {
+
+            Session::set("confirmPasswordResetMessage", "Please ensure both the new password and confirm password entries match.");
+        }
+    } else {
+
+        Session::set("confirmPasswordResetMessage", "Please ensure you have entered data in all the fields");
+    }
+}
+
 if (Input::getItemExists("userID") && Input::getItemExists("resetCode"))
 {
     $userID = Input::get("userID");
     $resetCode = Input::get("resetCode");
-    Session::set("confirmPasswordResetMessage", "Got the stuff");
+
+    $userQuery = new User();
+
+    $rows = $userQuery->checkForPendingPasswordReset($userID, $resetCode);
+    if($userQuery->db->getRowCount() > 0)
+    {
+        // Allow user to reset their password
+?>
+        <h1>Reset Password</h1>
+
+        <form method="post" action="">
+
+            <ul class="blank">
+                <li>
+                    <input type="password" placeholder="New Password" name="newPassword" />
+                </li>
+                <li>
+                    <input type="password" placeholder="Confirm New Password" name="passwordMatch" />
+                </li>
+                <li>
+                    <button type="submit" name="passwordSetAttempt"> Set New Password </button>
+                </li>
+                <input type="hidden" name="userID" value=<?= $userID?> />
+
+                <li class="confirmPasswordResetError">
+                    <?= Session::get("confirmPasswordResetMessage"); ?>
+                </li>
+            </ul>
+        </form>
+
+
+<?php        
+    }
+    else
+    {
+        Session::set("loginMessage", "No pending password request found.");
+        header("Location: login.php");
+    }
 }
 else
 {
@@ -31,6 +97,6 @@ else
 
 ?>
 
-<li class="confirmPasswordReset">
-    <?= Session::get("confirmPasswordResetMessage"); ?>
-</li>
+<?php
+Session::delete("confirmPasswordResetMessage");
+?>
--- Battleships/Content/Pages/header.php
+++ Battleships/Content/Pages/header.php
@@ -69,7 +69,7 @@ require_once("../Classes/setup.php");
     <div id="header">
 
         <div class="version">
-            Version: 2.10.3
+            Version: 2.10.4
         </div>
 
         <div id="headerBar">
